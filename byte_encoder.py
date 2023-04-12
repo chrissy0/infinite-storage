01a99a56579e5f2d1f10747f04d32481a9dfde72
Christopher Woggon <christopher.woggon@gmail.com>
2023-04-12 13:19:37 +0900

    Now faster and works reliably with youtube

--- byte_encoder.py
+++ byte_encoder.py
@@ -3,6 +3,7 @@ import shutil
 from os import listdir
 from os.path import isfile, join
 from pathlib import Path
+
 import cv2
 import numpy as np
 from PIL import Image
@@ -38,29 +39,58 @@ def encode_file(path):
         for bit in byte_to_bits(byte):
             bits.append(bit)
 
-    white_pixel = (255, 255, 255)
-    red_pixel = (255, 0, 0)
-    frames = []
+    frames_list = []
     counter = 0
     import math
     with tqdm(total=math.ceil(len(bits) / (height * width))) as pbar:
         while counter < len(bits):
+            all_rows_list = []
+            for row in range(height):
+                single_row_list = []
+                for col in range(width):
+                    to_append = None
+                    if counter < len(bits):
+                        bit = bits[counter]
+                        if bit == "1":
+                            to_append = False
+                        if bit == "0":
+                            to_append = True
+                    single_row_list.append(to_append)
+                    counter += 1
+                all_rows_list.append(single_row_list)
+            frames_list.append(all_rows_list)
+            pbar.update(1)
+
+    numpy_masks = []
+    for entry in tqdm(frames_list):
+        numpy_masks.append(np.array(entry))
+
+    white_pixel = (255, 255, 255)
+    red_pixel = (255, 0, 0)
+    for mask_idx, numpy_mask in enumerate(tqdm(numpy_masks)):
+        if mask_idx == len(numpy_masks) - 1:
             frame = Image.new('RGB', (width, height))
             pixels = frame.load()
             for row in range(height):
                 for col in range(width):
-                    if counter >= len(bits):
+                    val = numpy_mask[row][col]
+                    if val is None:
                         pixels[col, row] = red_pixel
                         continue
-                    bit = bits[counter]
-                    counter += 1
-                    if bit == "1":
+                    if val is True:
+                        pixels[col, row] = white_pixel
                         continue
-                    pixels[col, row] = white_pixel
-            pbar.update(1)
-            frame = frame.resize((width * scale_factor, height * scale_factor), Image.LANCZOS)
-            frame.save(f"out/{str(len(frames)).zfill(8)}.png")
-            frames.append(frame)
+        else:
+            frame = Image.new('RGB', numpy_mask.shape[::-1])
+
+            # set the pixel values based on the boolean values
+            arr = np.array(frame)
+            arr[:, :, 0] = numpy_mask * 255
+            arr[:, :, 1] = numpy_mask * 255
+            arr[:, :, 2] = numpy_mask * 255
+
+            frame = Image.fromarray(arr, mode='RGB')
+        frame.save(f"out/{str(mask_idx).zfill(8)}.png")
 
 
 def decode_frame(frame):
@@ -104,6 +134,7 @@ def decode_video(video_path, out_path):
 
     fps = cap.get(cv2.CAP_PROP_FPS)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(fps)
     frames_per_encoded_frame = fps / fps_encoded
     frames_loaded = 0
     frames_checked = 0
@@ -138,7 +169,12 @@ def create_video(out_path):
     images_read = sorted([f"out/{f}" for f in listdir(directory) if isfile(join(directory, f))])
 
     for img in tqdm(images_read):
-        video.write(cv2.cvtColor(np.array(Image.open(img)), cv2.COLOR_RGB2BGR))
+        arr = np.array(Image.open(img))
+        arr = np.repeat(arr, scale_factor, axis=0)
+        arr = np.repeat(arr, scale_factor, axis=1)
+
+        video.write(cv2.cvtColor(arr, cv2.COLOR_RGB2BGR))
+
     video.release()
 
 
